9eeb8552a8460fc9ebb762c756ceada8303616c6
DESKTOP-1GO7OG8\Tsend-Ayush <tsendayush0412@gmail.com>
2018-01-24 14:26:04 +0800

    template ready

---
+++ v5/v5/asdasd.blade.php
@@ -0,0 +1,4 @@
+@section('og-url'){!! https://lucklife1.playtravl.com/product/$product->id !!} @stop
+@section('og-title'){!! $product->name !!} @stop
+@section('og-description'){!! $product->description !!} @stop
+@section('og-image'){!! https://lucklife1.playtravl.com/uploads/product/$product->image !!} @stop
